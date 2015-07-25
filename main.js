69e25a333c5e401fd50cd32157dbac4ceab8f196
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-07-24 19:51:51 -0700

    add js file and define global variables

---
+++ main.js
@@ -0,0 +1,9 @@
+// Instantiate Global Variables
+var SoundArray = [];
+var record = false;
+var play = false;
+var startTime = 0;
+
+function playSound(button) {
+  new Audio(button + '.mp3').play();
+}
