f8f26ebc2b355bfa28034bac831ccd1d94b21e9c
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-07-24 20:37:30 -0700

    add pubnub and parse functions

--- main.js
+++ main.js
@@ -96,3 +96,96 @@ function updateBeats() {
   }
   $("#beats").html(html);
 }
+
+// PubNub
+
+pubnub = PUBNUB({
+  publish_key : 'your_pub_key'
+  subscribe_key : 'your_sub_key'
+});
+
+//Subscribe to channel 'demo'
+function subscribeTo() {
+  pubnub.subscribe({
+    channel: "demo",
+    //when message is received
+    message: function(m) {
+      // set data button and time to the received message
+      var data = {
+        button: m.button,
+        time: m.time
+      }
+      // push the received data into the SoundArray
+      SoundArray.push(data);
+      // call updateBeats to fix array
+      updateBeats();
+    },
+    connect: function(m) {
+      // load the song from Parse when we first subscribe
+      loadFromParse()
+    },
+    error: function(error) {
+      console.log(JSON.stringify(error));
+    }
+  });
+}
+
+function pubInit() {
+  subscribeTo();
+}
+pubInit();
+
+function publishBeat(time, button) {
+  pubnub.publish({
+    // set channel to demo
+    channel: "demo",
+    // our message is the beat we are playing
+    message: {
+      time: time,
+      button: button,
+    },
+    callback: function(m) {
+      console.log(m)
+    }
+  });
+}
+
+// Parse Functions
+
+function loadFromParse() {
+  SoundArray.length = 0;
+  var SoundObject = Parse.Object.extend("Sounds");
+  var query = new Parse.Query(SoundObject);
+  query.find({
+    success: function(song) {
+      if (song.length) {
+        for (var i = 0; i < song.length; i++) {
+          SoundArray = song[i].get("SoundAndTime");
+        }
+      }
+      updateBeats();
+    }
+  });
+}
+
+function saveToParse() {
+  var SoundObject = Parse.Object.extend("Sounds");
+  var query = new Parse.Query(SoundObject);
+  query.first({
+    success: function(song) {
+      if (song) {
+        song.save(null, {
+          success: function(songSave) {
+            songSave.set("SoundAndTime", SoundArray);
+            songSave.save();
+          }
+        });
+      }
+      else {
+        var soundObject = new SoundObject();
+        soundObject.set("SoundAndTime", SoundArray);
+        soundObject.save();
+      }
+    }
+  });
+}
